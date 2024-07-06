5ad32bf47a98820b681426cc62624a0c98d77c92
Aaron Thompson <aaronseanthompson@icloud.com>
2024-07-06 19:50:34 +0100

    Initial Commit

---
+++ Edutainment/EdutainmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  EdutainmentApp.swift
+//  Edutainment
+//
+//  Created by Aaron Thompson on 06/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct EdutainmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
